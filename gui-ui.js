b628bcbafa33838d7f57a428e1724b5cefc6d9c3
gui <agamennon@gmail.com>
2014-09-30 14:22:30 -0300

    indo

--- gui-ui.js
+++ gui-ui.js
@@ -7,7 +7,15 @@
 //angular.module('gui.directives');
 //angular.module('gui-ui', ['gui.directives']);
 
-angular.module('gui.directives', []);
-angular.module('gui.filters', []);
+//angular.module('gui.directives', []);
+//angular.module('gui.filters', []);
+//angular.module('utils', []);
+//angular.module('gui-ui', ['gui.directives','gui.filters','utils']);
+
+
+
+angular.module('gui.config', []).value('gui.config', {});
+angular.module('gui.filters', ['gui.config']);
+angular.module('gui.directives', ['gui.config']);
 angular.module('utils', []);
-angular.module('gui-ui', ['gui.directives','gui.filters','utils']);
+angular.module('gui-ui', ['gui.filters','gui.filters', 'gui.directives', 'gui.config']);
